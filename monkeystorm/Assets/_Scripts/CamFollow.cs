4e894c6ad40696dd3e313c3a380b5f296d8c48cf
DertyMunke <joel.abney@gmail.com>
2015-11-05 14:49:31 -0800

    Updated level 1 and JB_scene with new updates
    Added more branches and stretched the trees. Also, fixed the camera
    follow so that it clamps to the background image.

--- monkeystorm/Assets/_Scripts/CamFollow.cs
+++ monkeystorm/Assets/_Scripts/CamFollow.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CamFollow : MonoBehaviour{
+public class CamFollow : MonoBehaviour {
 
-    private void FixedUpdate()
+    private const float MAX_Y = 4.2f;
+    private const float MIN_Y = 1.22f;
+    private const float MAX_X = 17.8f;
+    private const float MIN_X = -17.75f;
+
+    public Transform player;
+
+    private void FixedUpdate ()
     {
-        //Mathf.Clamp(..y, 1.22f, 4.2f);
-        Debug.Log(transform.position.y);
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, MIN_X, MAX_X),
+                Mathf.Clamp(player.position.y, MIN_Y, MAX_Y), transform.position.z);
     }
 }
