35ade504b82fc80c80cb6793b6fa05417c5c6c8c
DertyMunke <joel.abney@gmail.com>
2015-11-05 13:27:52 -0800

    Integrated new monkey movements

---
+++ monkeystorm/Assets/_Scripts/CamFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour{
+
+    private void FixedUpdate()
+    {
+        //Mathf.Clamp(..y, 1.22f, 4.2f);
+        Debug.Log(transform.position.y);
+    }
+}
--- monkeystorm/Assets/_Scripts/Monkey.cs
+++ monkeystorm/Assets/_Scripts/Monkey.cs
@@ -4,20 +4,47 @@ using System.Collections;
 public class Monkey : MonoBehaviour {
     //left click variable instead of put 0
     public int leftClick = 0;
+    //check appropiate swipe length
+    public int swipeLengh = 40;
     //save mouse positions
     public Vector2 oldMousePosition;
     public Vector2 newMousePosition;
     //moving options
     public float moveSpeed;
     public float jumpHeight;
+    public float jumpS;
+    public float moveS;
+    //limit angle
+    public float limitAngle;
+    //ground check
+    public Transform branchCheck;
+    public float branchCheckRadius;
+    public LayerMask whatIsBranch;
+    private bool grounded;
+    //give up grap and hold back grap
+    Collider2D coll;
+    private bool pass;
 
     // Use this for initialization
     void Start()
     {
         oldMousePosition = Vector2.zero;
         newMousePosition = Vector2.zero;
-        moveSpeed = 8;
-        jumpHeight = 18;
+        moveSpeed = 10;
+        jumpHeight = 15;
+        jumpS = 1.5f;
+        moveS = 0.8f;
+        limitAngle = 70;
+        branchCheckRadius = 0.1f;
+        coll = GetComponent<Collider2D>();
+        pass = true;
+    }
+
+    //check per sec; use this for physics
+    void FixedUpdate()
+    {
+        //set grounded if it is attached to branches
+        grounded = Physics2D.OverlapCircle(branchCheck.position, branchCheckRadius, whatIsBranch);
     }
 
     // Update is called once per frame
@@ -27,8 +54,8 @@ public class Monkey : MonoBehaviour {
         if (GetDeltaMouse())
         {
             //move
-            Debug.Log("will move");
             MonkeyMove();
+           
             //reset the delta after move
             ResetMousePosition();
         }
@@ -47,7 +74,7 @@ public class Monkey : MonoBehaviour {
         {
             newMousePosition = Input.mousePosition;
             //get delta
-            return (Vector2.Distance(oldMousePosition,newMousePosition) > 60);
+            return (Vector2.Distance(oldMousePosition,newMousePosition) > swipeLengh);
         }
         //when only mouse down
         return false;        
@@ -66,25 +93,130 @@ public class Monkey : MonoBehaviour {
     //move the monkey by delta vector
     private void MonkeyMove()
     {
-        //get angle of swipe from stright right to swipe vector
+        //presets for directions
+        int rightD = 0;
+        int leftD = 1;
+        int straightD = 2;
+        int upD = 3;
+        int downD = 4;
+
+        //get the directions for up or down
+        int yD = GetYD(upD, downD);
+        //get the directions for right, left, straight
+        int xD = GetXD(rightD, leftD, straightD);
+
+        //if xD is right and if yD is up, right jump
+        //if xD is straight and if yD is up, straight up jump
+        //if xD is left and if yD is up, left jump
+        //if xD is left and if yD is down, left little move
+        //if xD is straight and if yD is down, drop
+        //if xD is right and if yD is down, right little move
+
+        
+        move(rightD, leftD, straightD, upD, downD, xD, yD);
+    }
+
+    //get the directions for right, left, straight 
+    private int GetXD(int r, int l, int s)
+    {
+        //get an angle of swipe from x-axis to swipe vector
         float angle = Vector2.Angle(Vector2.right, newMousePosition - oldMousePosition);
-        //if angle from 0 to 60, right jump
-        //if angle from 60 to 120, up jump
-        //if angle from 120 to 180, left jump
-        //if angle from 180 to 240, left drop
-        //if angle from 240 to 300, drop
-        //if angle from 300 to 360, right drop
-        if (angle >= 0 && angle < 60)
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, jumpHeight);
-        else if(angle >= 60 && angle < 120)
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
-        else if (angle >= 120 && angle < 180)
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, jumpHeight);
-        //else if (angle >= 180 && angle < 240)
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        //else if (angle >= 240 && angle < 300)
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y);
-        //else if (angle >= 300 && angle < 360)
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        //angle limits
+        float rightLimit = limitAngle;
+        float leftLimit = 180 - limitAngle;
+
+        //angle will be from 0 to 180, need direction or checking axis
+        if (angle < 0 || angle > 180)
+            Debug.Log("angle error");
+        //if angle from 0 to rightLimit, right
+        if (angle < rightLimit)
+            return r;
+        //if angle from 180 to leftLimit, left
+        else if (angle > leftLimit)
+            return l;
+        //if angle between limits, straight
+        else 
+            return s;
+
+    }
+
+    //get the directions for up or down
+    private int GetYD(int u, int d)
+    {
+        //the vector we will compute
+        Vector2 v = oldMousePosition - newMousePosition;
+
+        //if y <0, up; if y >0 down
+        if (v.y < 0)
+            return u;
+        else
+            return d;
+    }
+    
+    //move by directions
+    private void move(int rightD, int leftD, int straightD, int upD, int downD, int xD, int yD)
+    {
+        //jump if chacco is on branches
+        //up directions
+        if (yD == upD && grounded)
+        {
+            //if xD is right and if yD is up, right jump
+            if (xD == rightD)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, jumpHeight);
+            //if xD is straight and if yD is up, straight up jump
+            else if (xD == straightD)
+                StraightJump();
+            //if xD is left and if yD is up, left jump
+            else
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, jumpHeight);
+
+           
+        }
+        //down directions
+        else
+        {
+            //if xD is right and if yD is down, right little move
+            if (xD == rightD)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * moveS, GetComponent<Rigidbody2D>().velocity.y);
+            //if xD is straight and if yD is down, drop
+            else if (xD == straightD)
+                Drop();
+            //if xD is left and if yD is down, left little move
+            else
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed * moveS, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+
+    //drop move
+    private bool Drop()
+    {
+        //if it attached to branches, make monkey isTrigger
+        if(grounded)
+            coll.isTrigger = true;
+
+        //return true, if it is trigger; otherwise false
+        return coll.isTrigger;
+    }
+
+    //straight jump, if jump it will pass one branch and hold back the grap
+    private void StraightJump()
+    {
+        coll.isTrigger = true;
+        pass = false;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight * jumpS);
+    }
+
+    //whenever monkey enter the colliders, it means it will pass them
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        pass = true;
+    }
+    
+    //whenever monkey pass the collider, trigger set to false
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(pass)
+        coll.isTrigger = false;
     }
 }
