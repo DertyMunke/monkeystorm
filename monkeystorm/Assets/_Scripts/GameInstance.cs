f95c48317f18c47b3a0b108c966f84d62928478b
zhengwew <mikewzw@126.com>
2015-11-17 17:25:06 -0800

    UI_MERGING

--- monkeystorm/Assets/_Scripts/GameInstance.cs
+++ monkeystorm/Assets/_Scripts/GameInstance.cs
@@ -2,6 +2,12 @@
 
 public static class GameInstance
 {
+    //setting data
     public static float volum = 1;
     public static float brightness = 1;
+
+    //game data
+    public static float timer = 0;
+    public static int currentScore = 0;
+    public static int currentHP = 3;
 }
--- monkeystorm/Assets/_Scripts/UIEvents.cs
+++ monkeystorm/Assets/_Scripts/UIEvents.cs
@@ -2,24 +2,49 @@
 using System.Collections;
 using DG.Tweening;
 
-public class UIEvents : MonoBehaviour {
+public class UIEvents : MonoBehaviour
+{
+    //Main menu
     public GameObject mainMenu;
     public GameObject setting;
     public GameObject Record;
     public GameObject Rankings;
     public Texture[] BGTextures;
     public UITexture BG;
+    //Game menu
+    public GameObject gameMenu;
+    public UILabel timerLabel;
+    public UILabel scoreLabel;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public UITexture[] hearts;
+    // Use this for initialization
+    void Start()
+    {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.loadedLevelName != "Main Menu")
+        {
+            GameInstance.timer += Time.deltaTime;
+            timerLabel.text = ((int)GameInstance.timer).ToString();
+
+            scoreLabel.text = GameInstance.currentScore.ToString();
+
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                hearts[i].gameObject.SetActive(false);
+            }
+            for (int i = 0; i < GameInstance.currentHP; i++)
+            {
+                hearts[i].gameObject.SetActive(true);
+            }
+        }
+    }
+
+    #region MainMenu
     public void OnPlayClick()
     {
         Application.LoadLevel("Level1");
@@ -29,9 +54,9 @@ public class UIEvents : MonoBehaviour {
     {
         BG.mainTexture = BGTextures[1];
         mainMenu.SetActive(false);
-        setting.transform.DOMoveX(0, 0.3f);
+        setting.transform.DOLocalMoveX(0, 0.3f);
         setting.SetActive(true);
-        
+
     }
 
     public void OnBGMClick()
@@ -71,7 +96,7 @@ public class UIEvents : MonoBehaviour {
     public void OnMenuClick()
     {
         BG.mainTexture = BGTextures[0];
-        setting.transform.DOMoveX(795/360, 0.3f);
+        setting.transform.DOMoveX(795 / 360, 0.3f);
         setting.SetActive(false);
         mainMenu.transform.DOMoveX(0, 0.3f);
         mainMenu.SetActive(true);
@@ -81,7 +106,7 @@ public class UIEvents : MonoBehaviour {
     {
         BG.mainTexture = BGTextures[0];
         Record.SetActive(false);
-        Record.transform.DOMoveX(795/360, 0.3f);
+        Record.transform.DOMoveX(795 / 360, 0.3f);
         mainMenu.transform.DOMoveX(0, 0.3f);
         mainMenu.SetActive(true);
     }
@@ -91,5 +116,44 @@ public class UIEvents : MonoBehaviour {
         BG.mainTexture = BGTextures[1];
         setting.SetActive(false);
         Rankings.transform.DOMoveX(0, 0.3f);
+        Rankings.SetActive(true);
     }
+
+    public void OnRankingBackClick()
+    {
+        BG.mainTexture = BGTextures[0];
+        Rankings.SetActive(false);
+        Rankings.transform.DOMoveX(795 / 360, 0.3f);
+        mainMenu.transform.DOMoveX(0, 0.3f);
+        mainMenu.SetActive(true);
+    }
+    #endregion
+
+    #region GameMenu
+    public void OnPauseClick()
+    {
+        Time.timeScale = 0; // game pause
+        gameMenu.SetActive(true);
+    }
+
+    public void OnGameMenuPlayClick()
+    {
+        Time.timeScale = 1; //resume
+        gameMenu.SetActive(false);
+    }
+
+    public void OnSaveClick()
+    { }
+
+    public void OnMainMenuSettingClick()
+    {
+
+    }
+
+    public void OnQuitClick()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel("Main Menu");
+    }
+    #endregion
 }
