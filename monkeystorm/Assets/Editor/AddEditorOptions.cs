da628ad626d23439cae40d6aa3e86402ae10588e
DertyMunke <joel.abney@gmail.com>
2015-12-06 23:43:11 -0800

    Got the build to work! Yay!!

---
+++ monkeystorm/Assets/Editor/AddEditorOptions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class AddEditorOptions : MonoBehaviour {
+
+
+    [MenuItem("Edit/Reset Playerprefs")]
+
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
